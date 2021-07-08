b7de48cd73cbad1dee0241d109cc8ebd5304b5e6
alessandrofuda <alessandro.fuda@gmail.com>
2021-07-08 18:13:03 +0200

    publish saml2 package configs

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,7 +5,12 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Event;
+use Slides\Saml2\Events\SignedIn;
+use Slides\Saml2\Events\SignedOut;
+
+
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,7 +22,7 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
-        ],
+        ]
     ];
 
     /**
@@ -29,6 +34,22 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        //
+        Event::listen(SignedIn::class, function (SignedIn $event) {
+            $messageId = $event->getAuth()->getLastMessageId();
+
+            // your own code preventing reuse of a $messageId to stop replay attacks
+            $samlUser = $event->getSaml2User();
+
+            $userData = [
+                'id' => $user->getUserId(),
+                'attributes' => $user->getAttributes(),
+                'assertion' => $user->getRawSamlAssertion()
+            ];
+
+            $user = // find user by ID or attribute
+
+                // Login a user.
+                Auth::login($user);
+        });
     }
 }
